b6ff6e13074d678367f936134a3b2a58b2513536
Kevin Kwok <antimatter15@gmail.com>
2022-10-31 13:44:44 -0700

    vite bug workaround

--- src/worker/buffer.ts
+++ src/worker/buffer.ts
@@ -1,2 +1,13 @@
 import { Buffer } from "buffer-es6"
 export default Buffer
+
+// TODO: workaround for "?url" import in worker (cf. https://github.com/vitejs/vite/issues/9879)
+if (import.meta.env.PROD && typeof document === "undefined") {
+	Object.assign(globalThis, {
+		document: {
+			currentScript: {
+				src: self.location.href,
+			},
+		},
+	})
+}
