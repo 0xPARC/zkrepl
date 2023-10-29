8a2ebb4b1e302c9ef080dc319843455445a654f9
Divide-By-0 <aayushg@mit.edu>
2023-10-29 15:15:42 -0400

    now it works for circomlib/circuits and circomlib directly

--- src/worker/libraries.ts
+++ src/worker/libraries.ts
@@ -1,7 +1,11 @@
 export default function getLibraryUrlMap(): {[key: string]: string} {
+  // Note that this will first process circomlib/circuits, then circomlib, so
+  // both of them will point to the same place. This is intentional, as it
+  // allows people who skip the /circuits import in circomlib to still pass
   return {
     "gist:": "gist.github.com/",
-    "circomlib": "github.com/iden3/circomlib/blob/master",
+    "circomlib/circuits": "github.com/iden3/circomlib/blob/master/circuits",
+    "circomlib": "github.com/iden3/circomlib/blob/master/circuits",
     "@zk-email/circuits": "github.com/zkemail/zk-email-verify/tree/main/packages/circuits",
     "@zk-email/contracts": "github.com/zkemail/zk-email-verify/tree/main/packages/contracts",
     "@zk-email/helpers": "github.com/zkemail/zk-email-verify/tree/main/packages/helpers",
